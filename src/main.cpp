fc19fb1e06ca9e27a50c9af604c887f6bf131396
Rithvik Redrouthu <rithvik.redrouthu@gmail.com>
2025-03-29 19:08:12 -0400

    oh so you do need breaks?

--- src/main.cpp
+++ src/main.cpp
@@ -27,18 +27,18 @@ int main(int argc, char *argv[]) {
 
             for (char token : file_contents) {
                 switch (token) {
-                    case '(': std::cout << "LEFT_PAREN ( null" << std::endl;
-                    case ')': std::cout << "RIGHT_PAREN ) null" << std::endl;
-                    case '{': std::cout << "LEFT_BRACE { null" << std::endl;
-                    case '}': std::cout << "RIGHT_BRACE } null" << std::endl;
-                    case '+': std::cout << "PLUS + null" << std::endl;
-                    case '-': std::cout << "MINUS - null" << std::endl;
-                    case '*': std::cout << "STAR * null" << std::endl;
-                    case '/': std::cout << "SLASH / null" << std::endl;
-                    case '.': std::cout << "DOT . null" << std::endl;
-                    case ',': std::cout << "COMMA , null" << std::endl;
-                    case ';': std::cout << "SEMICOLON ; null" << std::endl;
-                    default: std::cerr << "[line 1] Error: Unexpected character: " << token << std::endl; code = 65;
+                    case '(': std::cout << "LEFT_PAREN ( null" << std::endl; break;
+                    case ')': std::cout << "RIGHT_PAREN ) null" << std::endl; break;
+                    case '{': std::cout << "LEFT_BRACE { null" << std::endl; break;
+                    case '}': std::cout << "RIGHT_BRACE } null" << std::endl; break;
+                    case '+': std::cout << "PLUS + null" << std::endl; break;
+                    case '-': std::cout << "MINUS - null" << std::endl; break;
+                    case '*': std::cout << "STAR * null" << std::endl; break;
+                    case '/': std::cout << "SLASH / null" << std::endl; break;
+                    case '.': std::cout << "DOT . null" << std::endl; break;
+                    case ',': std::cout << "COMMA , null" << std::endl; break;
+                    case ';': std::cout << "SEMICOLON ; null" << std::endl; break;
+                    default: std::cerr << "[line 1] Error: Unexpected character: " << token << std::endl; code = 65; break;
                 }
             }
         }
