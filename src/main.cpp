a1c29b5ebfd5322fee0d670f2b7ac7ac79645652
Rithvik Redrouthu <rithvik.redrouthu@gmail.com>
2025-03-29 18:37:44 -0400

    file contents

--- src/main.cpp
+++ src/main.cpp
@@ -20,11 +20,9 @@ int main(int argc, char *argv[]) {
 
     if (command == "tokenize") {
         std::string file_contents = read_file_contents(argv[2]);
-
-        std::cout<< file_contents << std::endl;
         
         if (!file_contents.empty()) {
-            std::cerr << "Scanner not implemented" << std::endl;
+            std::cout << file_contents << std::endl;
             return 1;
         }
         std::cout << "EOF  null" << std::endl; // Placeholder, remove this line when implementing the scanner
