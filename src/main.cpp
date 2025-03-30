76b79201a5d03b24eaa2895a75a02973e7fe7f15
Rithvik Redrouthu <rithvik.redrouthu@gmail.com>
2025-03-30 19:39:25 -0400

    perchance...

--- src/main.cpp
+++ src/main.cpp
@@ -49,8 +49,7 @@ int main(int argc, char *argv[]) {
                         }
                         if (i == file_contents.length()) {
                             std::cerr << "[line " << line << "] Error: Unterminated string." << std::endl;
-                            token_type = lexeme = literal = nullptr;
-                            code = 65;
+                            continue;
                         }
                         break;
                     case ' ': case '\t': break;
